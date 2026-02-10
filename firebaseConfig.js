d4b9235b15191fe6e5fe4be3b9225a13d6bee21f
rururu <62489807+rrurur@users.noreply.github.com>
2026-02-11 02:41:20 +0900

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyABKEbvIc5hSJzKrCSjAzFIhns5o_HhSkg",
+  authDomain: "furuyoni-diary-1918f.firebaseapp.com",
+  projectId: "furuyoni-diary-1918f",
+  storageBucket: "furuyoni-diary-1918f.firebasestorage.app",
+  messagingSenderId: "1031976761542",
+  appId: "1:1031976761542:web:da9bd7fe26790c8940ffc7"
+};
